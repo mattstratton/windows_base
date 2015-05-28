c21ff1b94d0e94aafa66260befc4529f533b7a2d
Matt Stratton <matt.stratton@gmail.com>
2015-05-28 00:28:52 -0500

    initial commit and add chef-client dependency

---
+++ metadata.rb
@@ -0,0 +1,9 @@
+name 'windows_base'
+maintainer 'Matt Stratton'
+maintainer_email 'mattstratton@chef.io'
+license 'apache2'
+description 'Installs/Configures windows_base'
+long_description 'Installs/Configures windows_base'
+version '0.1.0'
+
+depends 'chef-client', '~> 4.3.0'
